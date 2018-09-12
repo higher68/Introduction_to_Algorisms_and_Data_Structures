19f1fba019c2c4fbae404a87e652a71e06645de6
junya <hatiro7code@gmail.com>
2018-09-12 19:30:03 +0900

    20180911 AC but need thinking

--- ALDS1_3_B.py
+++ ALDS1_3_B.py
@@ -3,6 +3,22 @@ p = []
 for i in range(n):
     p_tmp = input().split()
     p.append([p_tmp[0], int(p_tmp[1])])
-print(n, q)
-for i in range(n):
-    print(i, p[i])
+# print(n, q)
+# for i in range(n):
+#     print(i, p[i])
+time = 0
+while len(p) != 0:
+    process_time = q
+    # print(time)
+    # print(process_time)
+    # print(p)
+    if p[0][1] > q:
+        p_tmp = p.pop(0)
+        p.append([p_tmp[0], p_tmp[1]-q])
+        time += process_time
+        process_time = 0
+    else:
+        process_time -= p[0][1]
+        time += p[0][1]
+        print(p[0][0], time)
+        p.pop(0)
