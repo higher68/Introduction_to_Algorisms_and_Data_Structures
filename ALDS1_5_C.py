9a6754b5fc6e5e26bc1a6793e0ff8b8d7224ff57
junya <hatiro7code@gmail.com>
2018-10-18 07:33:49 +0900

    20181018 wrote but need check

--- ALDS1_5_C.py
+++ ALDS1_5_C.py
@@ -1,5 +1,7 @@
 import math
 n = int(input())
+pi = math.acos(1)
+th = pi/3
 
 
 class point:
@@ -8,10 +10,25 @@ class point:
         self.y = y
 
 
-def koch(n, point_a, point_b):
+def koch(n, a, b):
         if n == 0:
             return 0
         s = point((2*a.x+1*b.x)/3, (2*a.y+1*b.y)/3)
         t = point((1*a.x+2*b.x)/3, (1*a.y+2*b.y)/3)
-        u = point((t.x-s.x)*math.cos(th)-(t.y-s.y)*math.sin(th)+s.x,
+        u = point((t.x-s.x)*math.cos(th)
+                  - (t.y-s.y)*math.sin(th)+s.x,
                   (t.x-s.x)*math.sin(th)-(t.y-s.y)*math.cos(th)+s.y)
+        koch(n-1, a, s)
+        print("{%.8f} {%.8f}".format(s.x, s.y))
+        koch(n-1, s, u)
+        print("{%.8f} {%.8f}".format(u.x, u.y))
+        koch(n-1, u, t)
+        print("{%.8f} {%.8f}".format(t.x, t.y))
+        koch(n-1, t, b)
+
+
+a = point(0, 0)
+b = point(100, 0)
+print("{%.8f} {%.8f}".format(a.x, a.y))
+koch(n, a, b)
+print("{%.8f} {%.8f}".format(b.x, b.y))
