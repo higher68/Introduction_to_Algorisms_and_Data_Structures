d4a1db23cbcfcd96fcb50d0ae37aeeb0d4c58b8e
junya <hatiro7code@gmail.com>
2018-10-24 08:09:14 +0900

    20181025 need debug WA

--- ALDS1_5_B.py
+++ ALDS1_5_B.py
@@ -1,18 +1,25 @@
 
 SENTINEL = 2000000000
 n = int(input())
-A = [int(_) for _ in input().split()]
-MAX = 5000000
-L = []*(MAX/2+2)
-R = []*(MAX/2+2)
+MAX = 500000
+A_tmp = input().split()
+print(A_tmp)
+A = [''] * MAX
+print(A[0:3])
+for i in range(n):
+    A[i] = int(A_tmp[i])
+L = ['']*(MAX//2+2)
+R = ['']*(MAX//2+2)
 cnt = 0
 
 
 def merge(A, n, left, mid, right):
+    global cnt
     n1 = mid - left
     n2 = right - mid
     for i in range(n1):
         L[i] = A[left + i]
+    for i in range(n2):
         R[i] = A[mid + i]
     L[n1] = SENTINEL
     R[n2] = SENTINEL
@@ -30,7 +37,7 @@ def merge(A, n, left, mid, right):
 
 def mergeSort(A, n, left, right):
     if left+1 < right:
-        mid = (left+right)/2
+        mid = (left+right)//2
         mergeSort(A, n, left, mid)
         mergeSort(A, n, mid, right)
         merge(A, n, left, mid, right)
@@ -38,5 +45,5 @@ def mergeSort(A, n, left, right):
 
 mergeSort(A, n, 0, n)
 
-print(A, sep=" ")
+print(A[:n], sep=" ")
 print(cnt)
