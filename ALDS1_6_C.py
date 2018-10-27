60058301ecd40efda9e962061e62a6b832de0579
junya <hatiro7code@gmail.com>
2018-10-27 09:43:57 +0900

    20161026 input bug & module bug fixed but need more

--- ALDS1_6_C.py
+++ ALDS1_6_C.py
@@ -1,19 +1,17 @@
 
 def merge(A, n, left, mid, right):
-    global cnt
     n1 = mid - left
     n2 = right - mid
     for i in range(n1):
         L[i] = A[left + i]
     for i in range(n2):
         R[i] = A[mid + i]
-    L[n1] = SENTINEL
-    R[n2] = SENTINEL
+    L[n1] = new_list("X", SENTINEL)
+    R[n2] = new_list("X", SENTINEL)
     i = 0
     j = 0
     for k in range(left, right):
-        cnt += 1
-        if L[i] <= R[j]:
+        if L[i].value <= R[j].value:
             A[k] = L[i]
             i += 1
         else:
@@ -30,10 +28,10 @@ def mergeSort(A, n, left, right):
 
 
 def partition(p, r):
-    x = A[r]
+    x = A[r].value
     i = p - 1  # <xの右端
     for j in range(p, r):
-        if A[j] <= x:
+        if A[j].value <= x:
             i += 1
             A_tmp = A[i]
             A[i] = A[j]
@@ -58,16 +56,30 @@ class new_list:
 
 
 n = int(input())
+S_tmp = [input().split() for i in range(n)]
 for i in range(n):
-    S_tmp = input().split()
+    print(S_tmp[i])
 A = []
 B = []
 for i in range(n):
-    A.append(new_list(S_tmp[0], S_tmp[1]))
-    B.append(new_list(S_tmp[0], S_tmp[1]))
+    A.append(new_list(S_tmp[i][0], int(S_tmp[i][1])))
+    B.append(new_list(S_tmp[i][0], int(S_tmp[i][1])))
+
+# for i in range(n):
+#     print(i, A[i].suit, A[i].value)
 
 L = [''] * (n//2+2)
 R = [''] * (n//2+2)
 SENTINEL = 2000000000
 mergeSort(A, n, 0, n)
 quickSort(B, n, 0, n)
+
+for i in range(n):
+    if A[i].suit != B[i].suit:
+        break
+if i == n-1:
+    print("Stable")
+else:
+    print("Not Stable")
+for i in range(n):
+    print(A[i].value)
