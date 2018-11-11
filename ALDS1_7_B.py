25f2e14ffe6e4f09b8d0268adbbda514c7656e8e
junya <hatiro7code@gmail.com>
2018-11-11 19:20:49 +0900

    20181111 wrote input

--- ALDS1_7_B.py
+++ ALDS1_7_B.py
@@ -5,12 +5,39 @@ class Node:
         self.right = right
 
 
+def PrintNodes(i):
+    print("node {}: ".formt(i), end="")
+    print("parent = {}, ".formt())
+    else:
+
+
+
+def recur(i_node, depth):
+    Depth[i_node] = depth
+    if Depth[i_node].left is not None:
+        recur(Depth[i_node].left, depth+1)
+    if Depth[i_node].right is not None:
+        recur(Depth[i_node].right, depth+1)
+
+
 n = int(input())
+Depth = [""] * n
 Nodes = [Node()] * n
 # input Node-info
 for i in range(n):
     q = [int(_) for _ in input().split()]
     if q[1] != -1:
-         Nodes[i].left = q[1]
+        Nodes[i].left = q[1]
+        Nodes[q[1]].parent = i
     if q[2] != -1:
         Nodes[i].right = q[2]
+        Nodes[q[2]].parent = i
+
+root_node = -11
+for i in range(n):
+    if Nodes[i].parent is None:
+        Nodes[i].parent = -1
+        rooot_node = i
+        break
+
+recur(r, 0)
