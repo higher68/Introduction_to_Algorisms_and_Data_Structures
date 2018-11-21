8dc953d3056cfe3a7df35c8ceb2121ce9f54a7f4
junya <hatiro7code@gmail.com>
2018-11-22 08:00:27 +0900

    20181122 insert writing

--- ALDS1_8_A.cpp
+++ ALDS1_8_A.cpp
@@ -21,8 +21,29 @@ void insert(int k){
 
   z = (Node *)malloc(sizeof(Node));  // pointerに対してのメモリの動的な割り当て。zポインタからNodeのポインタへ渡してるっぽい.二重割り当てか?
   // 確保したメモリをNode型ポインタへキャスト。構造体のsizeofは構造体メンバのバイト数
-  z->key = k  // zオブジェクトのポインタに対して参照
+  z->key = k;  // zオブジェクトのポインタに対して参照
   // zはNode型へのポインタ型変数。あるノードへのアドレスが入る
   // 上でzが指すNode用のメモリが確保
   // ここではzつまり確保したノードのアドレスのkeyがさすアドレスに変数を代入
+  z->left = NIL;
+  z->right = NIL;
 
+  while (x != NIL) {
+    y = x;
+    if (z->key < x->key) {
+      x = x->left;
+    } else {
+      x = x->right;
+    }
+  }
+  z->parent = y
+  while (x != NIL) {
+    y = x;
+    if (z->key < x->key) {
+      x = x->left;
+    } else {
+      x = x->right;
+    }
+  }
+  }
+  z->parent = y
