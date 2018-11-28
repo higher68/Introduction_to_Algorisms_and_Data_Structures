a4d36f1e1b53f31b90595efb71767cc2a86da83b
junya <hatiro7code@gmail.com>
2018-11-29 08:15:11 +0900

    20181130 writing main

--- ALDS1_8_C.cpp
+++ ALDS1_8_C.cpp
@@ -79,3 +79,22 @@ void preorder(Node *u) {
   prorder(u->right);
 }
 
+
+int main() {
+  int n, i, x;
+  string com;
+  scanf("%d", &n);
+
+  for (i = 0; i< n; i++) {
+    cin >> com;
+    if (com[0] == 'f' ){
+      scanf("%d", &x);
+      Node *t = find(root, x);
+      if (t != NIL)printf("yes\n");
+      else printf("no\n");
+    } else if (com == "print") {
+      inorder(root);
+      printf("\n");
+      preorder(root);
+
+
