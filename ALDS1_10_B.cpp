03cc145dd15e22534a8eefa66db27162859610d3
junya <hatiro7code@gmail.com>
2019-01-06 12:11:40 +0900

    20180106 wrote and need understand and check

--- ALDS1_10_B.cpp
+++ ALDS1_10_B.cpp
@@ -11,7 +11,23 @@ int main() {
     cin >> p[i-1] >> p[i];
   }
 
+  // 同じindex、つまり行列1個の場合はコスト0
   for (int i = 1; i <= n; i++) m[i][i] = 0;
+  // 計算する行列積の長さでループ
   for (int l = 2; l <= n; l++) {
+    // 計算し始める行列の始点をずらす
     for (int i = 1; i <= n -l + 1; i++ ) {
+      int j = i + l - 1;
+      // 1 の 21bit左シフト。要するにinfty
+      m[i][j] = (1 << 21);
+      for (int k = i; k <= j - 1; k++) {
+        m[i][j] = min(m[i][j], m[i][k] + m[k+1][j] + p[i - 1] * p[k] * p[j]);
+      }
+    }
+  }
+
+  cout << m[1][n] << encl;
+
+  return 0;
+}
 
