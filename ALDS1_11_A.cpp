0aca3d125b6b5f5ddb3933f76858ae05e399ee8e
junya <hatiro7code@gmail.com>
2019-01-13 12:48:26 +0900

    20190113 AC understood

--- ALDS1_11_A.cpp
+++ ALDS1_11_A.cpp
@@ -9,10 +9,30 @@ int main() {
   cin >> n;
   
   // initial condition insert
-  for (int i = 0; i < n; j++) {
+  for (int i = 0; i < n; i++) {
     for(int j = 0; j < n; j++) M[i][j] = 0;
   }
 
-  for (int i = 0; i < n; i++)
+  for (int i = 0; i < n; i++) {
+    cin >> u >> k;
+    u--; // 0オリジンへ変換
+    for (int j = 0; j < k; j++) {
+      cin >> v;
+      v--; // 0オリジンへ変換
+      M[u][v] = 1; // uとvの間に辺を振る
+    }
+  }
 
+  for (int i = 0; i < n; i++) {
+    for ( int j = 0; j < n; j++) {
+      if (j) cout << " "; // 最初以外は空白を前におく
+      cout << M[i][j];
+    }
+    cout << endl;
+  }
+  
+  return 0;
+}
+
+      
 
