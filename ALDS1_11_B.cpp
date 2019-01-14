31c66fee3e427f9770f7755760d558b55520c597
junya <hatiro7code@gmail.com>
2019-01-14 21:16:03 +0900

    c++ likeに変更

--- ALDS1_11_B.cpp
+++ ALDS1_11_B.cpp
@@ -1,23 +1,45 @@
 #include<stdio.h>
-#define N 100
-#define WHITE 0
-#define GRAY 1
-#define BLACK 2
+#include<stack>
+using namespace std;
+static const int N = 100;
+static const int WHITE = 0;
+static const int GRAY = 1;
+static const int BLACK = 2;
 
-int n, M[N][N]
+int n, M[N][N];
 int color[N], d[N], f[N], tt;
+int nt[N];            
 
-// 再帰関数による深さ優先探索
-void dfs_visit(int u) {
-  int v;
-  color[u] = GRAY;
-  d[u] = ++tt; // 最初の訪問
-  for (v = 0; v < n; v++) {
-    if ( M[u][v] == 0)  continue;
-    if (color[v] == WHITE ) {
-      dfs_visit(v);
-    }
+// u に隣接するvを番号順に取得
+int next(int u) {
+  for ( int v = nt[u]; v < n; v++ ) {
+    nt[u] = v + 1;
+    if (M[u][v] ) return v;
   }
-  color[u] = BLACK;
-  f[u] = ++tt // 訪問の完了
+  return -1;
 }
+
+// スタックを用いた深さ優先探索
+void dfs_visit(int r) {
+  for (int i = 0; i < n; i++) nt[i] = 0;
+
+  stack<int> S;
+  S.push(r);
+  color[r] = GRAY;
+  d[r] = ++tt;
+
+  while (!S.empty() ) {
+    int u = S.top();
+    int v = next(u);
+    if ( v!= -1) {
+      if (color[v] == WHITE) {
+        color[v] = GRAY;
+        d[v] = ++tt;
+        S.push(v);
+      }
+    } else {
+    }
+
+
+ }
+
