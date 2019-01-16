6d17d191e8d4fe05008de9a73ca9ebb41c8ab286
junya <hatiro7code@gmail.com>
2019-01-16 19:04:40 +0900

    20190116 AC understood

--- ALDS1_11_B.cpp
+++ ALDS1_11_B.cpp
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include<iostream>
 #include<stack>
 using namespace std;
 static const int N = 100;
-static const int WHITE = 0;
-static const int GRAY = 1;
-static const int BLACK = 2;
+static const int WHITE = 0; // 未訪問の状態
+static const int GRAY = 1; // 訪問中
+static const int BLACK = 2; // 訪問済み
 
 int n, M[N][N];
 int color[N], d[N], f[N], tt;
@@ -24,20 +24,23 @@ void dfs_visit(int r) {
   for (int i = 0; i < n; i++) nt[i] = 0;
 
   stack<int> S;
-  S.push(r);
+  S.push(r); // 始点uをスタックに追加
   color[r] = GRAY;
-  d[r] = ++tt;
+  d[r] = ++tt; // 訪問時刻を記載
 
-  while (!S.empty() ) {
-    int u = S.top();
-    int v = next(u);
+  while (!S.empty() ) { // Sが空でない間
+    int u = S.top(); // stackの一番上にある要素
+    int v = next(u); 
     if ( v!= -1) {
+      // topの隣に訪問していないものがあったら、スタックに追加
       if (color[v] == WHITE) {
         color[v] = GRAY;
         d[v] = ++tt;
         S.push(v);
       }
     } else {
+      // topの隣が何もなかったら、スタックから一番上をだす。
+      // fに時間を記載
       S.pop();
       color[u] = BLACK;
       f[u] = ++tt;
@@ -57,6 +60,7 @@ void dfs() {
 
   // 未訪問のuを始点として深さ優先探索
   for (int u = 0; u < n; u++ ) {
+    // 全頂点について、未訪問ならdfs_visit
     if ( color[u] == WHITE ) dfs_visit(u);
   }
   for (int i = 0; i < n; i++) {
@@ -68,4 +72,23 @@ void dfs() {
 int main() { 
   int u, k, v;
   cin >> n;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++ ) M[i][j] = 0;
+  }
 
+  for (int i = 0; i < n; i++ ){
+    cin >> u >> k;
+    // 0オリジンに
+    u--;
+    for (int j = 0; j < k; j++) {
+      cin >> v;
+      // 0オリジンに
+      v--;
+      M[u][v] = 1;
+    }
+  }
+
+  dfs();
+
+  return 0;
+}
