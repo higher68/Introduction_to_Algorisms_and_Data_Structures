9f577adecb7f7c172fa6b81351d2c5cfd90e22b9
junya <hatiro7code@gmail.com>
2019-01-20 16:51:35 +0900

    const, library定義

---
+++ ALDS1_12_A.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+static const int MAX = 100;
+static const int INFTY = (1<<21);
+static const int WHITE = 0;
+static const int GRAY = 1;
+static const int BLACK = 2;
