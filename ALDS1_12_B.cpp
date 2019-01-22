598bb52e54008adb7ef047ad01e40d279235938a
junya <hatiro7code@gmail.com>
2019-01-23 08:07:26 +0900

    実装完了。テスト必要。

--- ALDS1_12_B.cpp
+++ ALDS1_12_B.cpp
@@ -33,11 +33,38 @@ void dijkstra() {
     if ( u == -1) break;
     color[u] = BLACK;
     for (int v = 0; v < n; v++) {
-      if (color[v] != BLACK && M[u][v] !+ INFTY ) {
+      if (color[v] != BLACK && M[u][v] != INFTY ) {
         if (d[v] > d[u] + M[u][v] ) {
           d[v] = d[u] + M[u][v];
           color[v] = GRAY;
         }
       }
     }
-  
+  }
+ for (int i = 0; i < n; i++ ){
+  cout << i << " " << (d[i] == INFTY ? -1 : d[i] ) << endl;
+ }
+}
+
+int main() {
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    for(int j = 0; j < n; j++ ){
+      M[i][j] = INFTY;
+    }
+  }
+
+  int k, c, u, v;
+  for (int i = 0; i < n; i++ ){
+    cin >> u >> k;
+    for (int j = 0; j< k; j++) {
+      cin >> v >> c;
+      M[u][v] = c;
+    }
+  }
+
+  dijkstra();
+
+  return 0;
+}
+
