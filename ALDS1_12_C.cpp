5b8bba53d29912d589cd676a41ae8d752bd16574
junya <hatiro7code@gmail.com>
2019-01-25 23:43:14 +0900

    使用ライブラリ定義

---
+++ ALDS1_12_C.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<algorithm>
+#include<queue>
+using namespace std;
+static const int MAX = 100;
+static const int INFTY = (1 << 20);
+static const int WHITE = 0;
+static const int GRAY = 1;
+static const int BLACK = 2;
+
+
